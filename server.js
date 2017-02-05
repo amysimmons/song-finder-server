5ddc0d49f2106b0939888c4fa4664bd5ab307f28
Amy Simmons <amy__simmons@hotmail.com>
2017-02-05 22:21:54 +0000

    retrieving lyric data

--- server.js
+++ server.js
@@ -25,6 +25,8 @@ app.use(bodyParser.urlencoded({
     extended: true
 }));
 
+app.set('jsonp callback name', 'callback');
+
 app.post('/recognise', function(req, res) {
 
   fs.writeFile("out.raw", req.body.base64, 'base64', function(err) {
@@ -41,21 +43,33 @@ app.post('/recognise', function(req, res) {
 });
 
 app.post('/findsongs', function(req, res) {
-
-  var url = 'https://api.musixmatch.com/ws/1.1/track.search?format=jsonp&callback=callback&q_lyrics=' +
+  var url = 'https://api.musixmatch.com/ws/1.1/track.search?format=json&callback=callback&q_lyrics=' +
   req.body.transcription +
   '&quorum_factor=1&apikey=' +
   process.env.MUSIXMATCH_KEY
 
   request(url, function(err, response, body){
     if (!err && response.statusCode == 200) {
-      console.log(body);
+
+
       res.send(body);
+
+      var data = json.parse(body)
+
+      var title = encodeURIComponent(data.message.body.track_list[0].track.track_name);
+      var artist = encodeURIComponent(data.message.body.track_list[0].track.artist_name);
+
+      res.redirect(200, '/mainsong=' + title + artist);
     } else {
-      console.log(err, response);
+      console.log('err', err, 'response', response);
     }
   });
+});
 
+app.post('/mainsong', function(req, res) {
+  console.log('in main song')
+  console.log(req.query.valid)
+  console.log(req.params);
 });
 
 app.listen(3000);
